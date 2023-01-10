dbc8aca75b40b068c08b91e444c870bdb82df389
Rhea Ramachandran <rheara4@gmail.com>
2023-01-10 14:03:59 -0500

    Updates

---
+++ precache-manifest.bc8edfb814443b68a642ae0849a74501.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f73d2959eb56f132cfb",
+    "url": "/HealthyEats/static/css/main.23aaa2d8.chunk.css"
+  },
+  {
+    "revision": "4f73d2959eb56f132cfb",
+    "url": "/HealthyEats/static/js/main.4f73d295.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/HealthyEats/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/HealthyEats/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "0ecfa06670705884b62b",
+    "url": "/HealthyEats/static/js/runtime~main.0ecfa066.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/HealthyEats/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "62d99ef5f255919140935edfda12be71",
+    "url": "/HealthyEats/index.html"
+  }
+];
